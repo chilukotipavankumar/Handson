d6f88d7d7556034a0e39c0edc6fc25d66c5cabcb
chilukotipavankumar <77827810+chilukotipavankumar@users.noreply.github.com>
2021-05-02 16:02:07 +0530

    Add files via upload

---
+++ Nunit/Handson 7/NUnitMoqHandson7/NUnitMoqHandson7/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitMoqHandson7
+{
+    public class Calculator
+    {
+        public int Divide(int dividend, int devisor)
+        {
+            if (devisor == 0)
+                throw new ArgumentNullException("Devisor cannot be null");
+            return dividend / devisor;
+        }
+    }
+}
